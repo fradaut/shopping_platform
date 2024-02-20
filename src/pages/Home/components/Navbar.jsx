b3af10da4b53472c5804f1455b67e7abba3ad161
fradaut <qoo19830526@gmail.com>
2024-02-20 23:21:35 +0800

    feat: Add Navbar component and import into Home.jsx
    Created Navbar.jsx as a new component.
    Imported Navbar.jsx into Home.jsx for use in the homepage.

---
+++ src/pages/Home/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-black bg-opacity-80">
+      <div className="flex items-center">
+        <div className="text-white">nav</div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
